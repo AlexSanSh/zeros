767bc41f155868b16bbf363f9e0ed7fbedf30cbd
AlexSanSh <shpyra007@gmail.com>
2018-03-01 12:27:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let count = 0;
+  function isDevide(n){
+    if(n%5===0) {
+      count++;
+      isDevide(n/5);
+    }
+  }
+  for (var i = number; i >=1; i--) {
+    isDevide(i);
+  }
+  return count;
 }
+
